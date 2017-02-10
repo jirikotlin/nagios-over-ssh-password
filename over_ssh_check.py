f020799c5d7b9a67c5bd48a4bf519106a674c873
root <root@vs873.server4u.cz>
2017-02-10 11:19:36 +0100

    Initial commit

---
+++ over_ssh_check.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+'''
+Execute script via ssh with password.
+
+Author: Jiri Kotlin
+
+'''
+import sys
+import pexpect
+import argparse
+
+parser = argparse.ArgumentParser(description='Execute script via ssh.')
+
+required = parser.add_argument_group('required arguments')
+
+required.add_argument('-H', type=str, help="hostname or ip address", required=True, dest="hostname")
+required.add_argument('-l', type=str, help="login to ssh on host", required=True, dest="login")
+required.add_argument('-p', type=str, help="password to ssh", required=True, dest="password")
+required.add_argument('-c', type=str, help="command to execute", required=True, dest="command")
+
+args = parser.parse_args()
+
+try:
+    try:
+        child = pexpect.spawn("ssh {0}@{1} {2}".format(args.login, args.hostname, args.command))
+        i = child.expect(['assword:'])
+        child.sendline(args.password)
+    except pexpect.EOF:
+        ret =  "CRITICAL - unable to login to sshi at {0}".format(args.hostname)
+        print ret
+        sys.exit(2)
+
+    i = child.expect([pexpect.EOF])
+    if i == 0:
+        ret = child.before.strip()
+        print ret
+        sys.exit(0)
+    else:
+        print "CRITICAL - unable to read output."
+        sys.exit(2)
+except pexpect.TIMEOUT:
+    print "CRITICAL - request timed out"
+    sys.exit(2)
+
